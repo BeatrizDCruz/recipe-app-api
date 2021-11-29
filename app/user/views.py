ff692475da92eaad84e150dc4f9167f76cce6f7e
Beatriz Cruz <mllebia@live.com>
2021-11-29 11:04:07 -0300

    Added endpoint for creating and authenticating users

---
+++ app/user/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for users"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
