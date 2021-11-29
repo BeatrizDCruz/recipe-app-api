1bc84214d12e916940330731478cb6947884f53b
Beatriz Cruz <mllebia@live.com>
2021-11-29 16:26:48 -0300

    Corrected Styling

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,11 +18,11 @@ class UserSerializer(serializers.ModelSerializer):
             }
         }
 
-        def create(self, validated_data):
+    def create(self, validated_data):
             """Create a new user with encrypted password and return it"""
             return get_user_model().objects.create_user(**validated_data)
 
-        def update(self, instance, validated_data):
+    def update(self, instance, validated_data):
             """Update a user, setting the password correctly and return it"""
             password = validated_data.pop('password', None)
             user = super().update(instance, validated_data)
